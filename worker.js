7e2d91da77c8b4735df56344884a5fc5e0b2ee20
kevim <k.v.n.such@gmail.com>
2023-05-10 21:39:06 -0300

    Vite deploy

--- worker.js
+++ worker.js
@@ -10,8 +10,47 @@ const logHtml = function (cssClass, ...args) {
 const log = (...args) => logHtml('', ...args);
 const error = (...args) => logHtml('error', ...args);
 
+let root_sqlite3;
 const start = function (sqlite3) {
+    root_sqlite3 = sqlite3;
     log('Running SQLite3 version', sqlite3.version.libVersion);
+    let db = start_db(sqlite3);
+    try {
+        log('Creating a table...');
+        db.exec('CREATE TABLE IF NOT EXISTS t(a,b)');
+        log('Insert some data using exec()...');
+        for (let i = 20; i <= 25; ++i) {
+            insert(db, 'INSERT INTO t(a,b) VALUES (?,?)', [i, i * 2])
+        }
+        select(db, 'SELECT a FROM t ORDER BY a LIMIT 3');
+    } finally {
+        db.close();
+    }
+};
+
+
+function insert(db, sql, args){
+    db.exec({
+        sql,
+        bind: args,
+    });
+}
+
+function select(db, sql){
+    let rows = [];
+    db.exec({
+        sql,
+        callback: (row) => {
+            rows.push(row);
+            log(row);
+        },
+    });
+    log('Query data with exec()...');
+    console.log(rows);
+    return rows;
+}
+
+function start_db(sqlite3){
     let db;
     if ('opfs' in sqlite3) {
         db = new sqlite3.oo1.OpfsDb('/mydb.sqlite3');
@@ -20,27 +59,17 @@ const start = function (sqlite3) {
         db = new sqlite3.oo1.DB('/mydb.sqlite3', 'ct');
         log('OPFS is not available, created transient database', db.filename);
     }
-    try {
-        log('Creating a table...');
-        db.exec('CREATE TABLE IF NOT EXISTS t(a,b)');
-        log('Insert some data using exec()...');
-        for (let i = 20; i <= 25; ++i) {
-            db.exec({
-                sql: 'INSERT INTO t(a,b) VALUES (?,?)',
-                bind: [i, i * 2],
-            });
-        }
-        log('Query data with exec()...');
-        db.exec({
-            sql: 'SELECT a FROM t ORDER BY a LIMIT 3',
-            callback: (row) => {
-                log(row);
-            },
-        });
-    } finally {
-        db.close();
+    return db;
+}
+
+addEventListener('message', function (e){
+    const data = e.data;
+    if('listar' == data.cmd){
+        const db = start_db(root_sqlite3);
+        select(db, 'SELECT a FROM t ORDER BY a LIMIT 3');
     }
-};
+    console.log(data);
+})
 
 log('Loading and initializing SQLite3 module...');
 sqlite3InitModule({
